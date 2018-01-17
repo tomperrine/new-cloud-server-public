21f65274601c0aff56898b89ffdced2c7184010a
Tom Perrine <tom.perrine@gmail.com>
2018-01-17 13:46:21 -0800

    first commit

---
+++ install-server.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# this will copied onto and run on the host that's being installed
+# all that should be there is the base OS (must have git client)
+
+# must run as root, obviously
+
+# dont return from here until the OS is running, all packages have been installed and
+# the ansible update has happened
+
+# update the base OS
+apt-get update
+apt-get upgrade
+
+# make sure git is present, might as well get ansible while we're at it
+apt-get install git ansible
+
+
